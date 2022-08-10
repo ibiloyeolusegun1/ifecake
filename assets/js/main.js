9bae9f73c8c444dab9e718842aa145803dc88f9c
ibiloyeolusegun1 <104324212+ibiloyeolusegun1@users.noreply.github.com>
2022-08-09 21:48:21 -0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,34 @@ window.onscroll = () =>{
 
 
 
-
+/*================= Search box toggle =================*/
 let search = document.querySelector('.search');
 document.querySelector('#search').onclick=() =>{
     search.classList.toggle('active');
+}
+
+/*================= Whatsapp toggle =================*/
+let loginForm = document.querySelector('.chat-form-container');
+document.querySelector('#whatsapp-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+document.querySelector('#close-whatsapp-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+function whatsapp(){
+    var name = document.getElementById("Wname").value;
+    var phone = document.getElementById("Wphone").value;
+    var email = document.getElementById("Wemail").value;
+    var message = document.getElementById("Wmessage").value;
+
+    var url = "https:/wa.me/09033382694?text="
+    +"Name : "+Wname+"%0a"
+    +"Phone : "+Wphone+"%0a"
+    +"Email : "+Wemail+"%0a"
+    +"Message : "+Wmessage+"%0a"
+
+    window.open(url, '_blank').focus();
 }
 
 
